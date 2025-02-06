f777f7c1669a5f80b03d7fc199491849588bd317
cesarbaquiro <cmbm.kf@gmail.com>
2025-02-06 18:51:21 -0500

    Add connection

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 import express from "express";
 import { PORT } from "./src/config/config.js";
+import cors from "cors";
+import db from "./db.js";
 
 const app = express();
 
-// Ruta básica
-app.get("/", (req, res) => {
-    res.send("¡Vehicles CRUD backend is running!");
-  });
+// Allows requests only from http://localhost:4200
+app.use(cors({ origin: "http://localhost:4200" }));
+
+// Basic route
+app.get("/", async (req, res) => {
+  try {
+    const [rows] = await db.query("SELECT * FROM vehicle"); // Asynchronous database querying
+    if (!rows.length) {
+      console.log("No vehicles found");
+      return res.status(404).send("No vehicles found"); 
+    }
+    console.log(rows); // Print the vehicles to the console
+    res.json(rows); // Send response JSON
+  } catch (err) {
+    console.error('Error al obtener los vehículos:', err);
+    res.status(500).send('Error en la consulta de base de datos'); // Send an error response
+  }
+});
+
 
 const httpServer = app.listen(PORT, () => {
     console.log(`Server reunning on Port http://localhost:${PORT}`);
